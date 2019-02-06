771ac28ba05697fc32b1a10c7ce7a33f11ad8695
temelt <tanertemel1@gmail.com>
2019-02-06 20:34:15 +0300

    logging

--- src/main/java/com/temelt/issuemanagement/advice/IMExceptionHandler.java
+++ src/main/java/com/temelt/issuemanagement/advice/IMExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.temelt.issuemanagement.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +16,12 @@ import java.util.Date;
  */
 @ControllerAdvice
 @RestController
+@Slf4j
 public class IMExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<?> handleExceptions(Exception ex, WebRequest request) {
+        log.error("ControllerAdvice -> ExceptionHandler -> " , ex ,request);
         ExceptionResponse  exceptionResponse =new ExceptionResponse(new Date(),ex.getMessage());
         return new ResponseEntity<>(exceptionResponse , HttpStatus.EXPECTATION_FAILED);
     }
---
+++ src/main/java/com/temelt/issuemanagement/api/ProjectVersionedApi.java
@@ -0,0 +1,39 @@
+package com.temelt.issuemanagement.api;
+
+import com.temelt.issuemanagement.dto.ProjectDto;
+import com.temelt.issuemanagement.service.impl.ProjectServiceImpl;
+import com.temelt.issuemanagement.util.ApiPaths;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by temelt on 6.02.2019.
+ */
+@RestController
+@RequestMapping("/versioning")
+@Api(value = ApiPaths.ProjectCtrl.CTRL, description = "Project APIs")
+public class ProjectVersionedApi {
+
+    @Autowired
+    private ProjectServiceImpl projectServiceImpl;
+
+    @GetMapping(value = "/{id}",params = "version=1")
+    @ApiOperation(value = "Get By Id Operation V1", response = ProjectDto.class)
+    public ResponseEntity<ProjectDto> getByIdV1(@PathVariable(value = "id", required = true) Long id) {
+        ProjectDto projectDto = projectServiceImpl.getById(id);
+        return ResponseEntity.ok(projectDto);
+    }
+
+    @GetMapping(value = "/{id}", params = "version=2")
+    @ApiOperation(value = "Get By Id Operation V2", response = ProjectDto.class)
+    public ResponseEntity<ProjectDto> getByIdV2(@PathVariable(value = "id", required = true) Long id) {
+        ProjectDto projectDto = projectServiceImpl.getById(id);
+        return ResponseEntity.ok(projectDto);
+    }
+}
