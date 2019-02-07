c81ec48cb85dfc1a2def2c053ec2bd8be71e2303
temelt <tanertemel1@gmail.com>
2019-02-07 21:57:26 +0300

    UI-template-implementation

---
+++ src/webui/src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import {DashboardRoutingModule} from "./dashboard.routing.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DashboardRoutingModule
+  ],
+  declarations: [DashboardComponent]
+})
+export class DashboardModule { }
